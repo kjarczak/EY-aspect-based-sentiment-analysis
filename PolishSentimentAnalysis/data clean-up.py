7c66e22e19409bd49d6c28085055ec90dd336504
Kacper Jarczak <48131726+kjarczak@users.noreply.github.com>
2021-01-22 13:04:52 +0100

    Create data clean-up.py

---
+++ PolishSentimentAnalysis/data clean-up.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import re
+
+def azureml_main(dataframe1 = None, dataframe2 = None):
+    # dataframe1['content'] = removeSymbols(removeUrl(removeEmoji(dataframe1['content'])))
+    return dataframe1,
+
+def removeEmoji(txt):
+    emoji_pattern = re.compile("["
+                               u"\U0001F600-\U0001F64F"  # emoticons
+                               u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                               u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                               u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                               u"\U00002500-\U00002BEF"  # chinese char
+                               u"\U00002702-\U000027B0"
+                               u"\U00002702-\U000027B0"
+                               u"\U000024C2-\U0001F251"
+                               u"\U0001f926-\U0001f937"
+                               u"\U00010000-\U0010ffff"
+                               u"\u2640-\u2642"
+                               u"\u2600-\u2B55"
+                               u"\u200d"
+                               u"\u23cf"
+                               u"\u23e9"
+                               u"\u231a"
+                               u"\ufe0f"  # dingbats
+                               u"\u3030"
+                               "]+", flags=re.UNICODE)
+    return emoji_pattern.sub(r'', txt)
+
+def removeUrl(txt):
+    urls_pattern = re.compile('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')                          
+    return urls_pattern.sub(r'', txt)
+
+def removeSymbols(txt):
+    symbols_pattern = re.compile('[^AaĄąBbCcĆćDdEeĘęFfGgHhIiJjKkLlŁłMmNnŃńOoÓóPpRrSsŚśTtUuWwYyZzŹźŻż]+' u'')                          
+    return symbols_pattern.sub(r' ', txt)
