8e5d11b182fd78c79cd06e7308977d04a196287d
CzarekMichalski <49158661+CzarekMichalski@users.noreply.github.com>
2021-01-22 12:57:33 +0100

    Update getStats.js

--- static/scripts/getStats.js
+++ static/scripts/getStats.js
@@ -1,3 +1,4 @@
+``
 document.addEventListener("DOMContentLoaded", function () {
     let inputForm = document.getElementById("inputForm");
     inputForm.addEventListener("submit", function (event) {
@@ -14,6 +15,7 @@ document.addEventListener("DOMContentLoaded", function () {
             if (response.status === 200) {
                 response.json().then(data => {
                     getPlots(data);
+                    getTweetsExample(data);
                 });
             } else {
                 console.log('error');
@@ -67,5 +69,58 @@ document.addEventListener("DOMContentLoaded", function () {
                 });
             });
         }
+
+        function getTweetsExample(data) {
+            appendTableRows();
+            let positiveIndices = [];
+            let negativeIndices = [];
+            let neutralIndices = [];
+            let sentimentList = [];
+            let contentList = [];
+            for (let i in data.sentiment) {
+                sentimentList.push(data.sentiment[i]);
+            }
+            for (let i in data.content) {
+                contentList.push(data.content[i]);
+            }
+            for (let i = 0; i < sentimentList.length; i++) {
+                if (sentimentList[i] === -1 && negativeIndices.length < 3) {
+                    negativeIndices.push(i);
+                } else if (sentimentList[i] === 0 && neutralIndices.length < 3) {
+                    neutralIndices.push(i);
+                } else if (sentimentList[i] === 1 && positiveIndices.length < 3) {
+                    positiveIndices.push(i);
+                }
+            }
+            for (let i = 0; i < negativeIndices.length; i++) {
+                appendTweet(-1, i, contentList[negativeIndices[i]]);
+            }
+            for (let i = 0; i < neutralIndices.length; i++) {
+                appendTweet(0, i, contentList[neutralIndices[i]]);
+            }
+            for (let i = 0; i < positiveIndices.length; i++) {
+                appendTweet(1, i, contentList[positiveIndices[i]]);
+            }
+        }
+
+        function appendTableRows() {
+            let table = document.getElementById("table-body");
+            for (let i = 0; i < 3; i++) {
+                let row = document.createElement("tr");
+                row.id = "row-" + i;
+                for (let j = -1; j < 2; j++) {
+                    let column = document.createElement("td");
+                    column.id = "column-" + i + "-" + j;
+                    row.appendChild(column);
+                }
+                table.appendChild(row);
+            }
+        }
+
+        function appendTweet(sentiment, rowNo, content) {
+            let column = document.getElementById("column-" + rowNo + "-" + sentiment);
+            column.innerText = content;
+        }
     });
 });
+``
